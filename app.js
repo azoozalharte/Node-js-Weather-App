7e145da157286647d0d2d22e8925bb24aa5bd9fb
azooz alharte <zezo50115@protonmail.com>
2020-03-26 13:46:26 +0300

    test mapbox to give me latitude/longitude

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const request = require('request');
 
 const url =
-  'https://api.darksky.net/forecast/f25ba583af3cc25e491d0ee14b206a4a/37.8267,-122.4233';
+  'https://api.darksky.net/forecast/f25ba583af3cc25e491d0ee14b206a4a/37.8267,-122.4233?units=si';
 
 request({ url: url, json: true }, (err, res) => {
-  // console.log(res.body.timezone);
+  console.log(res.body.timezone);
   console.log(
-    `it's currently ${res.body.currently.temperature} degrees out. there is a ${res.body.currently.precipProbability}% to rain in ${res.body.timezone}`
+    `${res.body.daily.data[0].summary} it's currently ${res.body.currently.temperature} degrees out. there is a ${res.body.currently.precipProbability}% to rain in ${res.body.timezone}`
+  );
+});
+
+const url1 =
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXpvb3phbGhhcnRlIiwiYSI6ImNrNzZvbGY1dDAwMWszbm1mbXhvanp4cW0ifQ.iBQ0yrnUBA0MC0YO01aL7g&limit=1';
+
+request({ url: url1, json: true }, (err, res) => {
+  console.log(
+    `lat ${res.body.features[0].center[1]}, long ${res.body.features[0].center[0]}`
   );
 });
