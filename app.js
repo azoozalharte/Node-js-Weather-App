a6f2444c4f7f57b25fbff74a2e691451dd53e99e
azooz alharte <zezo50115@protonmail.com>
2020-03-31 02:03:54 +0300

    get the input from the user with yargs

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
-const request = require('request');
+const yargs = require('yargs').argv;
+
 const geocode = require('./utils/geocode');
-// // const url =
-// //   'https://api.darksky.net/forecast/f25ba583af3cc25e491d0ee14b206a4a/37.8267,-122.4233?units=si';
+const darksky = require('./utils/darksky');
 
-// // request({ url: url, json: true }, (err, res) => {
-// //   if (err) {
-// //     console.log('Unable to connect to weater service');
-// //   } else {
-// //     console.log(
-// //       `${res.body.daily.data[0].summary} it's currently ${res.body.currently.temperature} degrees out. there is a ${res.body.currently.precipProbability}% to rain in ${res.body.timezone}`
-// //     );
-// //   }
-// // });
-
-// const url1 =
-//   'https://api.mapbox.com/geocoding/v5/mapbox.places/12azoozka.json?access_token=pk.eyJ1IjoiYXpvb3phbGhhcnRlIiwiYSI6ImNrNzZvbGY1dDAwMWszbm1mbXhvanp4cW0ifQ.iBQ0yrnUBA0MC0YO01aL7g&limit=1';
-
-// request({ url: url1, json: true }, (err, res) => {
-//   if (err) {
-//     console.log('err');
-//   } else if (
-//     Array.isArray(res.body.features) &&
-//     res.body.features.length === 0
-//   ) {
-//     console.log('No match');
-//   } else {
-//     console.log(
-//       `lat ${res.body.features[0].center[1]}, long ${res.body.features[0].center[0]}`
-//     );
-//   }
-// });
-
-geocode('boston', (err, data) => {
-  console.log('err', err);
-  console.log('data', data);
-});
+if (yargs.location) {
+  geocode(yargs.location, (err, data) => {
+    console.log('place name: ', data.location);
+    darksky(data.lat, data.long, (err, darkskyData) => {
+      if (err) {
+        console.log('error', err);
+      } else {
+        console.log('darkskyData', darkskyData);
+      }
+    });
+  });
+} else {
+  console.log('please enter a location');
+}
